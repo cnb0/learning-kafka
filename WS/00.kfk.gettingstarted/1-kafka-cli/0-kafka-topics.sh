61f3580f15f9f7ad10cb18a5ef52224a6d668b44
cnb0 <devopstpt@gmail.com>
2020-11-25 05:31:34 +0000

    adding self balancing properties

--- WS/00.kfk.gettingstarted/1-kafka-cli/0-kafka-topics.sh
+++ WS/00.kfk.gettingstarted/1-kafka-cli/0-kafka-topics.sh
@@ -1,7 +1,7 @@
 # Replace "kafka-topics" 
 # by "kafka-topics.sh" or "kafka-topics.bat" based on your system # (or bin/kafka-topics.sh or bin\windows\kafka-topics.bat if you didn't setup PATH / Environment variables)
 
-kafka-topics.sh
+/home/nobleprog/kafka_2.12-2.4.0/bin/kafka-topics.sh
 
 kafka-topics --zookeeper 127.0.0.1:2181 --list 
 
